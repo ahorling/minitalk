77428e9a3bd030d10a282d1ac3de7b294c05d3b5
Alexander derck Horlings <horlingsalexander@gmail.com>
2022-11-04 20:30:14 +0100

    fixed server leak

--- sources/server/server.c
+++ sources/server/server.c
@@ -6,20 +6,40 @@
 /*   By: ahorling <ahorling@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/30 22:21:22 by ahorling      #+#    #+#                 */
-/*   Updated: 2022/11/04 19:33:48 by ahorling      ########   odam.nl         */
+/*   Updated: 2022/11/04 20:28:02 by ahorling      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minitalk.h"
 
-void	create_string(char c)
+char	*string_set(char c, char *string)
 {
-	static char	*string;
-	static int	i = 0;
-	char		cha[2];
+	char	cha[2];
+	char	*ret;
 
 	cha[0] = c;
 	cha[1] = '\0';
+	if (string == NULL)
+	{
+		ret = malloc(2 * sizeof(char));
+		if (!ret)
+			return (NULL);
+		ret[0] = c;
+		ret[1] = '\0';
+	}
+	else
+	{
+		ret = ft_strjoin(string, cha);
+		free(string);
+	}
+	return (ret);
+}
+
+void	create_string(char c)
+{
+	static char	*string;
+	char		*temp;
+
 	if (c == '\0')
 	{
 		ft_printf("%s\n", string);
@@ -27,17 +47,8 @@ void	create_string(char c)
 		string = NULL;
 		return ;
 	}
-	if (string == NULL)
-	{
-		string = malloc(2 * sizeof(char));
-		if (!string)
-			return ;
-		string[0] = c;
-		string[1] = '\0';
-	}
 	else
-		string = ft_strjoin(string, cha);
-	i++;
+		string = string_set(c, string);
 	return ;
 }
 
